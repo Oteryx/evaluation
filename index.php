204abdccd3423ce1c1d4af126d1b3db1afeab070
Oteryx <alexandre.noiriel@hotmail.com>
2020-12-07 15:05:11 +0100

    1
    Mise en place du style de la page ainsi que du css

---
+++ index.php
@@ -0,0 +1,82 @@
+<header>
+    <meta charset="UTF-8">
+    <title>Billeterie Fafouloux</title>
+    <link href="css/style.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
+          integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+</header>
+<body style="background-color: #4d004d; !important;">
+<div class="fond">
+    <div class="test1">
+        <div class="test">
+            Evenements à venir
+        </div>
+    </div>
+    <div class="carte">
+        <div class="card" style="width: 20rem;">
+            <img src="https://www.w3.org/html/logo/downloads/HTML5_sticker.png" class="card-img-top"
+                 alt="Accroche HTML">
+            <div class="card-body">
+                <h2 class="card-title">Titre</h2>
+                <h3 class="card-subtitle">Sous titre</h3>
+                <p class="card-text">Du texte sous le titre dans le corps de carte</p>
+                <a href="#" class="btn btn-primary">Un lien</a>
+            </div>
+        </div>
+        <div class="card" style="width: 20rem;">
+            <img src="https://www.w3.org/html/logo/downloads/HTML5_sticker.png" class="card-img-top"
+                 alt="Accroche HTML">
+            <div class="card-body">
+                <h2 class="card-title">Titre</h2>
+                <h3 class="card-subtitle">Sous titre</h3>
+                <p class="card-text">Du texte sous le titre dans le corps de carte</p>
+                <a href="#" class="btn btn-primary">Un lien</a>
+            </div>
+        </div>
+        <div class="card" style="width: 20rem;">
+            <img src="https://www.w3.org/html/logo/downloads/HTML5_sticker.png" class="card-img-top"
+                 alt="Accroche HTML">
+            <div class="card-body">
+                <h2 class="card-title">Titre</h2>
+                <h3 class="card-subtitle">Sous titre</h3>
+                <p class="card-text">Du texte sous le titre dans le corps de carte</p>
+                <a href="#" class="btn btn-primary">Un lien</a>
+            </div>
+        </div>
+    </div>
+    <div class="carte1">
+        <div class="card" style="width: 20rem;">
+            <img src="https://www.w3.org/html/logo/downloads/HTML5_sticker.png" class="card-img-top"
+                 alt="Accroche HTML">
+            <div class="card-body">
+                <h2 class="card-title">Titre</h2>
+                <h3 class="card-subtitle">Sous titre</h3>
+                <p class="card-text">Du texte sous le titre dans le corps de carte</p>
+                <a href="#" class="btn btn-primary">Un lien</a>
+            </div>
+        </div>
+        <div class="card" style="width: 20rem;">
+            <img src="https://www.w3.org/html/logo/downloads/HTML5_sticker.png" class="card-img-top"
+                 alt="Accroche HTML">
+            <div class="card-body">
+                <h2 class="card-title">Titre</h2>
+                <h3 class="card-subtitle">Sous titre</h3>
+                <p class="card-text">Du texte sous le titre dans le corps de carte</p>
+                <a href="#" class="btn btn-primary">Un lien</a>
+            </div>
+        </div>
+        <div class="card" style="width: 20rem;">
+            <img src="https://www.w3.org/html/logo/downloads/HTML5_sticker.png" class="card-img-top"
+                 alt="Accroche HTML">
+            <div class="card-body">
+                <h2 class="card-title">Titre</h2>
+                <h3 class="card-subtitle">Sous titre</h3>
+                <p class="card-text">Du texte sous le titre dans le corps de carte</p>
+                <a href="#" class="btn btn-primary">Un lien</a>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+<footer>
+</footer>
