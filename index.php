fd46d961b190d254f8d51b6fefa32afd3441644b
Oteryx <alexandre.noiriel@hotmail.com>
2020-12-08 15:43:18 +0100

    5
    Modification du css pour créé les classes css pour mettre en page

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <link href="css/style.css" rel="stylesheet">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
           integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link href="https://fonts.google.com/specimen/Space+Mono">
 </header>
 <body style="background-color: #4d004d; !important;">
 <div class="fond">
@@ -16,31 +17,41 @@
         <div class="card" style="width: 20rem;">
             <img src="img/201812_Perreault.jpg" class="card-img-top"
                  alt="Accroche HTML">
+            <div class="tourne">
+                <div class="tournetext">La tête dans les étoiles</div>
+            </div>
+            <div class="resume">12 janvier 2021 16h00<br><div class="heure">6 heures de folie<br></div><div class="place"> il reste 22 places</div></div>
             <div class="card-body">
-                <h2 class="card-title">Evénement 1</h2>
-                <h3 class="card-subtitle">Sous titre</h3>
-                <p class="card-text">Du texte sous le titre dans le corps de carte</p>
-                <a href="#" class="btn btn-primary">Un lien</a>
+                <div class="prix">C'est gratuit !!!</div>
+                <div class="description"><p class="card-text">Du texte sous le titre dans le corps de carte bla blabla bla bla bla bla bla Du texte sous le titre dans le corps de carte bla blabla bla bla bla bla bla </p></div>
+                <div class="bouton"><a href="#" class="btn btn-primary">Un lien</a></div>
             </div>
         </div>
         <div class="card" style="width: 20rem;">
             <img src="img/201812_Perreault.jpg" class="card-img-top"
                  alt="Accroche HTML">
-            <div class="card-body">
-                <h2 class="card-title">Evénement 2</h2>
-                <h3 class="card-subtitle">Sous titre</h3>
-                <p class="card-text">Du texte sous le titre dans le corps de carte</p>
-                <a href="#" class="btn btn-primary">Un lien</a>
+            <div class="tourne">
+                <div class="tournetext">La tête dans les étoiles</div>
+            </div>
+            <div class="resume">12 janvier 2021 16h00<br><div class="heure">6 heures de folie<br></div><div class="place"> </div></div>
+            <div class="card-body" style="background-color: white">
+                <div class="prix"></div>
+                <div class="complet">C'est complet ! sorry !</div>
+                <div class="description"><p class="card-text"></p></div>
+                <div class="bouton"></div>
             </div>
         </div>
         <div class="card" style="width: 20rem;">
             <img src="img/201812_Perreault.jpg" class="card-img-top"
                  alt="Accroche HTML">
+            <div class="tourne">
+                <div class="tournetext">La tête dans les étoiles</div>
+            </div>
+            <div class="resume">12 janvier 2021 16h00<br><div class="heure">6 heures de folie<br></div><div class="place"> il reste 22 places</div></div>
             <div class="card-body">
-                <h2 class="card-title">Evénement 3</h2>
-                <h3 class="card-subtitle">Sous titre</h3>
-                <p class="card-text">Du texte sous le titre dans le corps de carte</p>
-                <a href="#" class="btn btn-primary">Un lien</a>
+                <div class="prix">C'est gratuit !!!</div>
+                <div class="description"><p class="card-text">Du texte sous le titre dans le corps de carte bla blabla bla bla bla bla bla Du texte sous le titre dans le corps de carte bla blabla bla bla bla bla bla </p></div>
+                <div class="bouton"><a href="#" class="btn btn-primary">Un lien</a></div>
             </div>
         </div>
     </div>
@@ -48,31 +59,41 @@
         <div class="card" style="width: 20rem;">
             <img src="img/201812_Perreault.jpg" class="card-img-top"
                  alt="Accroche HTML">
+            <div class="tourne">
+                <div class="tournetext">La tête dans les étoiles</div>
+            </div>
+            <div class="resume">12 janvier 2021 16h00<br><div class="heure">6 heures de folie<br></div><div class="place"> il reste 22 places</div></div>
             <div class="card-body">
-                <h2 class="card-title">Evénement 4</h2>
-                <h3 class="card-subtitle">Sous titre</h3>
-                <p class="card-text">Du texte sous le titre dans le corps de carte</p>
-                <a href="#" class="btn btn-primary">Un lien</a>
+                <div class="prix">C'est gratuit !!!</div>
+                <div class="description"><p class="card-text">Du texte sous le titre dans le corps de carte bla blabla bla bla bla bla bla Du texte sous le titre dans le corps de carte bla blabla bla bla bla bla bla </p></div>
+                <div class="bouton"><a href="#" class="btn btn-primary">Un lien</a></div>
             </div>
         </div>
         <div class="card" style="width: 20rem;">
             <img src="img/201812_Perreault.jpg" class="card-img-top"
                  alt="Accroche HTML">
+            <div class="tourne">
+                <div class="tournetext">La tête dans les étoiles</div>
+            </div>
+            <div class="resume">12 janvier 2021 16h00<br><div class="heure">6 heures de folie<br></div><div class="place"> il reste 22 places</div></div>
             <div class="card-body">
-                <h2 class="card-title">Evénement 5</h2>
-                <h3 class="card-subtitle">Sous titre</h3>
-                <p class="card-text">Du texte sous le titre dans le corps de carte</p>
-                <a href="#" class="btn btn-primary">Un lien</a>
+                <div class="prix">C'est gratuit !!!</div>
+                <div class="description"><p class="card-text">Du texte sous le titre dans le corps de carte bla blabla bla bla bla bla bla Du texte sous le titre dans le corps de carte bla blabla bla bla bla bla bla </p></div>
+                <div class="bouton"><a href="#" class="btn btn-primary">Un lien</a></div>
             </div>
         </div>
         <div class="card" style="width: 20rem;">
             <img src="img/201812_Perreault.jpg" class="card-img-top"
                  alt="Accroche HTML">
-            <div class="card-body">
-                <h2 class="card-title">Evénement 6</h2>
-                <h3 class="card-subtitle">Sous titre</h3>
-                <p class="card-text">Du texte sous le titre dans le corps de carte</p>
-                <a href="#" class="btn btn-primary">Un lien</a>
+            <div class="tourne">
+                <div class="tournetext">La tête dans les étoiles</div>
+            </div>
+            <div class="resume">12 janvier 2021 16h00<br><div class="heure">6 heures de folie<br></div><div class="place"> </div></div>
+            <div class="card-body" style="background-color: white">
+                <div class="prix"></div>
+                <div class="complet">C'est complet ! sorry !</div>
+                <div class="description"><p class="card-text"></p></div>
+                <div class="bouton"></div>
             </div>
         </div>
     </div>
